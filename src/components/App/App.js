dd91f05322a2a55b11d8c1f760af7ec4c11f986a
Marifel Angeles <mmarifel@gmail.com>
2019-02-22 14:04:32 -0600

    add html components in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,27 @@ class App extends Component {
     return (
       <div>
         <Header />
+
+        <div>
+          <p>Enter a number and click up or down. The total will increase or decrease by that amount.</p>
+          <button>Up</button>
+          <input type="text" placeholder="Enter Number"/>
+          <button>Down</button>
+        </div>
+
+        <div>
+          <p>5</p>
+          <button>Save</button>
+          <p>Click save above to save the current total in the history</p>
+        </div>
+        
+        <div>
+          <h2>History</h2>
+          <ul>
+            <li>5</li>
+          </ul>
+        </div>
+        
       </div>
     );
   }
